39aa4cf954b99f0eff37c31149e16bc9b94205cf
Erika Soares da Silva <erikasons@gmail.com>
2018-03-23 17:32:08 -0300

    feat: structure the app to contain the navigators and tabs.

--- App.js
+++ App.js
@@ -1,23 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+import { StyleSheet, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Decks from './components/Decks';
+import NewDeck from './components/NewDeck';
+import DeckDetail from './components/DeckDetail';
+import NewCard from './components/NewCard';
+import reducer from './reducers';
+import Quiz from './components/Quiz';
+import { setLocalNotification } from './utils/helpers';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+const Tabs = TabNavigator({
+  Decks: {
+    screen: Decks,
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      title: 'New Deck',
+    },
+  },
+});
+
+const FlashcardNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  DeckDetail: {
+    screen: DeckDetail,
+    navigationOptions: ({ navigation }) => ({
+      title: `${navigation.state.params.title}`,
+    }),
+  },
+  NewCard: {
+    screen: NewCard,
+    navigationOptions: ({ navigation }) => ({
+      title: `${navigation.state.params.title}`,
+    }),
+  },
+  Quiz: {
+    screen: Quiz,
+  },
+});
+
+export default class App extends React.Component {
+  componentDidMount() {
+    setLocalNotification();
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={styles.container}>
+          <FlashcardNavigator />
+        </View>
+      </Provider>
+    );
+  }
+}
