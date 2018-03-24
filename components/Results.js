776c1cc61aeccc0a3ee0dafbbde1f21df21774cc
Erika Soares da Silva <erikasons@gmail.com>
2018-03-24 09:00:50 -0300

    refactor: add the navigation buttons

--- components/Results.js
+++ components/Results.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { connect } from 'react-redux';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { clearLocalNotification, setLocalNotification } from '../utils/helpers';
 
 const styles = StyleSheet.create({
@@ -17,7 +16,19 @@ const styles = StyleSheet.create({
     marginRight: 15,
     marginLeft: 15,
   },
-
+  actionBtn: {
+    height: 40,
+    width: '80%',
+    margin: 20,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  btnText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingTop: 8,
+  },
 });
 
 class Results extends Component {
@@ -30,17 +41,19 @@ class Results extends Component {
       <View style={styles.container}>
         <Text style={styles.phrase}>Você finalizou o quiz:</Text>
         <Text style={[styles.phrase, { marginTop: 20, color: 'gray', fontSize: 16 }]}>
-Você acertou {this.props.score} de {this.props.total}
+Você acertou {this.props.score} de {this.props.cardsTotal}
         </Text>
+
+        <TouchableOpacity style={[styles.actionBtn, { backgroundColor: 'black' }]} onPress={this.props.restartQuiz}>
+          <Text style={[styles.btnText, { color: 'white' }]}> Restart Quiz </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.actionBtn} onPress={this.props.backToDeck}>
+          <Text style={styles.btnText}> Back to Deck</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
-function mapStateToProps(deckReducer) {
-  return {
-    total: deckReducer.selectedDeck.cards.length,
-  };
-}
-
-export default connect(mapStateToProps)(Results);
+export default Results;
